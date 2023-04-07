6aa0ede01eb4f4f4231ba058a04e0d70b1fc1a69
ahmad rivaldi <rivaldi19122@gmail.com>
2023-04-08 00:59:07 +0700

    setup inertia done

---
+++ resources/ts/pages/student/home-student.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+export default function HomeStudent()
+{
+    return (
+        <>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed a doloribus dolorem debitis. Earum at facilis minima neque impedit obcaecati fuga? Ex voluptatem, aut nisi incidunt quaerat pariatur ad cumque!</p>
+        </>
+    );
+}
