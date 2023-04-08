0ce46a137b75ffa71429abe8570f9dc1b2c41bae
ahmad rivaldi <rivaldi19122@gmail.com>
2023-04-08 19:36:11 +0700

    remove unused

--- resources/ts/app.tsx
+++ resources/ts/app.tsx
@@ -4,18 +4,19 @@ import '../css/app.css';
 import React from 'react';
 import { createInertiaApp } from '@inertiajs/react'
 import { createRoot } from 'react-dom/client'
-import route from 'ziggy-js';
+
 
 createInertiaApp({
-    title: (title:string) => `${title} - e-learning Universitas Pamulang`,
-    progress:{
-        color:'#eb4034'
+    title: (title: string) => `${title} - e-learning`,
+    progress: {
+        color: '#eb4034'
     },
     resolve: name => {
-        const pages = import.meta.glob('./pages/**/*.tsx', { eager: true })
+        // @ts-ignore
+        const pages = import.meta.glob('./pages/**/*.tsx', {eager: true})
         return pages[`./pages/${name}.tsx`]
     },
-    setup({ el, App, props }) {
+    setup({el, App, props}) {
         createRoot(el).render(<App {...props} />)
     },
-})
+}).then( () =>{ })
